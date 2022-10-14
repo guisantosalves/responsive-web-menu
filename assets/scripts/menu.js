c11c36b43c4dd1d1425c57c43e8872a98eaf703f
guisantosalves <workguisantos@gmail.com>
2022-10-13 21:41:52 -0400

    setting animation

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -4,12 +4,27 @@ const menuIcon = document.querySelector('#menu-icon');
 menuIcon.addEventListener('click', ()=>{
     menu.classList.toggle("show");
     
-    // verify if show has the show class
+    // verify if menu has the show class
     if(menu.classList.contains('show')){
         menuIcon.src = '/assets/images/iconCloseMenu.svg';
         menuIcon.title = "Fechar menu"
     }else{
         menuIcon.src = '/assets/images/menu-hambuger.svg'
         menuIcon.title = "Abrir menu"
+    }
+})
+
+addEventListener('resize', () => {
+    // verify if menu has the show class
+    if(menu.classList.contains('show')){
+        menuIcon.src = '/assets/images/iconCloseMenu.svg';
+        menuIcon.title = "Fechar menu"
+    }else{
+        menuIcon.src = '/assets/images/menu-hambuger.svg'
+        menuIcon.title = "Abrir menu"
+    }
+
+    if(window.innerWidth > 768){
+        menu.classList.remove('show');
     }
 })
